65018a0ead78fc7e4553ea99192bfea5a5adced5
partypete25 <partypete25@hotmail.com>
2016-06-06 14:29:17 +1000

    Create calledscript.js

---
+++ calledscript.js
@@ -0,0 +1,12 @@
+function getString(x) {
+  var y = x;
+  var s;
+  if (y == "Adam") {
+    s = "Hello Sir";
+  } else if (y == "Eve") {
+    s = "Hello Miss";
+  } else {
+    s = "Hello";
+  }
+  return s;
+}
