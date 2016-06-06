c8f99a6be245a56fc354883cd9fe51a08cb13f80
partypete25 <partypete25@hotmail.com>
2016-06-06 16:19:54 +1000

    Update get_months.js

--- get_months.js
+++ get_months.js
@@ -2,6 +2,12 @@
   var months;
   var language = x;
   switch(language) {
+    case "af":
+      months = 'Jan_Feb_Mar_Apr_Mei_Jun_Jul_Aug_Sep_Okt_Nov_Des'.split('_');
+      break;   
+    case "ar":
+      
+      break;
     case "ar":
       months = 'يناير_فبراير_مارس_أبريل_مايو_يونيو_يوليو_أغسطس_سبتمبر_أكتوبر_نوفمبر_ديسمبر'.split('_');
       break;
