c540b96c10e046791bb159482fa76bf4386084df
partypete25 <partypete25@hotmail.com>
2016-06-06 14:44:35 +1000

    Update and rename calledscript.js to get_months.js

---
+++ get_months.js
@@ -0,0 +1,15 @@
+ getMonths(x){
+  var months, language = x;
+  switch(x) {
+    case "ar":
+      months = 'يناير_فبراير_مارس_أبريل_مايو_يونيو_يوليو_أغسطس_سبتمبر_أكتوبر_نوفمبر_ديسمبر'.split('_');
+      break;
+    case "ru":
+      months = 'января_февраля_марта_апреля_мая_июня_июля_августа_сентября_октября_ноября_декабря'.split('_');
+      break;
+    case "nl":
+      months = 'januari_februari_maart_april_mei_juni_juli_augustus_september_oktober_november_december'.split('_');
+      break;
+  }
+  return months;
+ }
